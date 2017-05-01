15686bf41a522de32eb3d06880cf57d0d0975774
blueskyleader01 <blueskyleader01@hotmail.com>
2017-05-01 11:17:53 -0700

    Initial commit

---
+++ BadDesignerLoad/BadControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BadDesignerLoad
+{
+    public partial class BadControl : UserControl
+    {
+        public BadControl()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Red;
+            File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "got you.txt"), "Code was run");
+        }
+    }
+}
